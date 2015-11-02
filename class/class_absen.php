b9066e4f9c3db361ff3ffb71b46b54ea40bcead1
bungamelia <bungamelia@hotmail.com>
2015-11-02 10:19:17 +0700

    Message

--- class/class_absen.php
+++ class/class_absen.php
@@ -1,104 +1,114 @@
 <?php
 	
-	class absen
+class absen
+{
+	protected $obj;
+	function __construct()
 	{
-		protected $obj;
-		function __construct()
+		$this->obj = new db;
+	}
+	
+	public function add_absen($id_karyawan, $id_shift, $waktu, $status)
+	{
+		try
 		{
-			$this->obj = new db;
-		}
-		
-		public function add_absen($id_karyawan, $id_shift, $waktu, $status){
-			try{
-				$query = "INSERT INTO absen(id_karyawan, id_shift, waktu, status) VALUES 
-						(:id_karyawan, :id_shift, :waktu, :status)";
-				
-				$this->obj->query($query);
-				$this->obj->bind(':id_karyawan',$id_karyawan);
-				$this->obj->bind(':id_shift',$id_shift);
-				$this->obj->bind(':waktu',$waktu);
-				$this->obj->bind(':status',$status);
-				$this->obj->execute();
-
-			}
-			catch(PDOException $e){
-				echo $e->getMessage();
-			} 
-		}
+			$query = "INSERT INTO absen(id_karyawan, id_shift, waktu, status) VALUES (:id_karyawan, :id_shift, :waktu, :status)";
 			
-		function getID_absen($id_karyawan){
-			$query = "SELECT * FROM absen WHERE id_karyawan=:id_karyawan ORDER BY waktu DESC LIMIT 1";
 			$this->obj->query($query);
 			$this->obj->bind(':id_karyawan',$id_karyawan);
+			$this->obj->bind(':id_shift',$id_shift);
+			$this->obj->bind(':waktu',$waktu);
+			$this->obj->bind(':status',$status);
 			$this->obj->execute();
-			$data = $this->obj->single();
-			return $data;
-		}
-		
-		public function absen_list($karyawan_id){
-			$query = "SELECT * FROM absen a, karyawan k WHERE a.id_karyawan = k.id_karyawan AND a.id_karyawan=:karyawan_id ORDER BY id_absen DESC";
-			$this->obj->query($query);
-			$this->obj->bind(':karyawan_id',$karyawan_id);
-			$this->obj->execute();
-			$data = $this->obj->resultset();
-			return $data;
-		}
-		
-		function absen_history($id_karyawan, $dari, $sampai){
-			$query = "SELECT * FROM absen a, karyawan k WHERE a.id_karyawan=:id_karyawan AND a.id_karyawan=k.id_karyawan AND waktu >= :dari AND waktu <= :sampai
-			ORDER BY id_absen DESC";
-			$this->obj->query($query);
-			$this->obj->bind(':id_karyawan',$id_karyawan);
-			$this->obj->bind(':dari',$dari);
-			$this->obj->bind(':sampai',$sampai);
-			$this->obj->execute();
-			$data = $this->obj->resultset();
-			return $data;
-		}
-		
-		function absenBy_date($dari, $sampai){
-			$query = "SELECT * FROM absen a, karyawan k WHERE a.id_karyawan=k.id_karyawan AND waktu >= :dari AND waktu <= :sampai ORDER BY id_absen DESC";
-			$this->obj->query($query);
-			$this->obj->bind(':dari',$dari);
-			$this->obj->bind(':sampai',$sampai);
-			$this->obj->execute();
-			$data = $this->obj->resultset();
-			return $data;
-		}
-		
-		function getAll_absen(){
-			$query = "SELECT * FROM absen a, karyawan k WHERE a.id_karyawan=k.id_karyawan ORDER BY id_absen DESC";
-			$this->obj->query($query);
-			$this->obj->execute();
-			$data = $this->obj->resultset();
-			return $data;
-		}
-		
-		function cetakAbsen($id_karyawan, $dari, $sampai){
-			$query = "SELECT * FROM absen a, karyawan k WHERE a.id_karyawan=:id_karyawan AND a.id_karyawan=k.id_karyawan AND waktu >= :dari AND waktu <= :sampai
-			ORDER BY id_absen DESC";
-			$this->obj->query($query);
-			$this->obj->bind(':id_karyawan',$id_karyawan);
-			$this->obj->bind(':dari',$dari);
-			$this->obj->bind(':sampai',$sampai);
-			$this->obj->execute();
-			$data = $this->obj->resultset();
-			return $data;
-		}
 
-		function edit_absen($absen_id, $id_karyawan, $waktu, $status){
-			try{
-				$query = "UPDATE absen SET waktu=:waktu, status=:status WHERE id_absen=:absen_id";
-				$this->obj->query($query);
-				$this->obj->bind(":waktu",$waktu);
-				$this->obj->bind(":status",$status);
-				$this->obj->bind(":absen_id",$absen_id);
-				$this->obj->execute();
-			}
-			catch(PDOException $e)
-			{
-				echo $e->getMessage(); 
-			}
+		}
+		catch(PDOException $e)
+		{
+			echo $e->getMessage();
+		} 
+	}
+		
+	function getID_absen($id_karyawan)
+	{
+		$query = "SELECT * FROM absen WHERE id_karyawan=:id_karyawan ORDER BY waktu DESC LIMIT 1";
+		$this->obj->query($query);
+		$this->obj->bind(':id_karyawan',$id_karyawan);
+		$this->obj->execute();
+		$data = $this->obj->single();
+		return $data;
+	}
+	
+	public function absen_list($karyawan_id)
+	{
+		$query = "SELECT * FROM absen a, karyawan k WHERE a.id_karyawan = k.id_karyawan AND a.id_karyawan=:karyawan_id ORDER BY id_absen DESC";
+		$this->obj->query($query);
+		$this->obj->bind(':karyawan_id',$karyawan_id);
+		$this->obj->execute();
+		$data = $this->obj->resultset();
+		return $data;
+	}
+	
+	function absen_history($id_karyawan, $dari, $sampai)
+	{
+		$query = "SELECT * FROM absen a, karyawan k WHERE a.id_karyawan=:id_karyawan AND a.id_karyawan=k.id_karyawan AND waktu >= :dari AND waktu <= :sampai
+		ORDER BY id_absen DESC";
+		$this->obj->query($query);
+		$this->obj->bind(':id_karyawan',$id_karyawan);
+		$this->obj->bind(':dari',$dari);
+		$this->obj->bind(':sampai',$sampai);
+		$this->obj->execute();
+		$data = $this->obj->resultset();
+		return $data;
+	}
+	
+	function absenBy_date($dari, $sampai)
+	{
+		$query = "SELECT * FROM absen a, karyawan k WHERE a.id_karyawan=k.id_karyawan AND waktu >= :dari AND waktu <= :sampai ORDER BY id_absen DESC";
+		$this->obj->query($query);
+		$this->obj->bind(':dari',$dari);
+		$this->obj->bind(':sampai',$sampai);
+		$this->obj->execute();
+		$data = $this->obj->resultset();
+		return $data;
+	}
+	
+	function getAll_absen()
+	{
+		$query = "SELECT * FROM absen a, karyawan k WHERE a.id_karyawan=k.id_karyawan ORDER BY id_absen DESC";
+		$this->obj->query($query);
+		$this->obj->execute();
+		$data = $this->obj->resultset();
+		return $data;
+	}
+	
+	function cetakAbsen($id_karyawan, $dari, $sampai)
+	{
+		$query = "SELECT * FROM absen a, karyawan k WHERE a.id_karyawan=:id_karyawan AND a.id_karyawan=k.id_karyawan AND waktu >= :dari AND waktu <= :sampai
+		ORDER BY id_absen DESC";
+		$this->obj->query($query);
+		$this->obj->bind(':id_karyawan',$id_karyawan);
+		$this->obj->bind(':dari',$dari);
+		$this->obj->bind(':sampai',$sampai);
+		$this->obj->execute();
+		$data = $this->obj->resultset();
+		return $data;
+	}
+
+	function edit_absen($absen_id, $id_karyawan, $waktu, $status)
+	{
+		try
+		{
+			$query = "UPDATE absen SET waktu=:waktu, status=:status WHERE id_absen=:absen_id";
+			$this->obj->query($query);
+			$this->obj->bind(":waktu",$waktu);
+			$this->obj->bind(":status",$status);
+			$this->obj->bind(":absen_id",$absen_id);
+			$this->obj->execute();
+		}
+		catch(PDOException $e)
+		{
+			echo $e->getMessage(); 
 		}
 	}
+}
 ?>
--- class/class_shift.php
+++ class/class_shift.php
@@ -1,97 +1,107 @@
 <?php
 	
-	class shift
+class shift
+{
+	protected $obj;
+	function __construct()
 	{
-		protected $obj;
-		function __construct()
-		{
-			$this->obj = new db;
-		}
-		
-		public function add_shift($nama_shift, $start_shift, $end_shift){
-			try{
-				$query = "INSERT INTO absen(nama_shift, start_shift, end_shift) VALUES 
-						(:nama_shift, :start_shift, :end_shift)";
-				
-				$this->obj->query($query);
-				$this->obj->bind(':nama_shift',$nama_shift);
-				$this->obj->bind(':start_shift',$start_shift);
-				$this->obj->bind(':end_shift',$end_shift);
-				$this->obj->execute();
-			}
-			catch(PDOException $e){
-				echo $e->getMessage();
-			}    
-		}
-		
-		public function tampil_shift(){
-			$query = "SELECT * FROM shift";
-			$this->obj->query($query);
-			$this->obj->execute();
-			$data = $this->obj->resultset();
-			return $data;
-		}
-		
-		function getID_shift($id_shift){
-			$query = "SELECT * FROM shift WHERE id_shift=:id_shift";
-			$this->obj->query($query);
-			$this->obj->bind(':id_shift',$id_shift);
-			$this->obj->execute();
-			$data = $this->obj->single();
-			return $data;
-		}
+		$this->obj = new db;
+	}
 	
-		function edit_shift($shift_id, $nama_shift, $start_shift, $end_shift){
-			try{
-				$query = "UPDATE shift SET nama_shift=:nama_shift, start_shift=:start_shift, end_shift=:end_shift WHERE id_shift=:shift_id";
-				$this->obj->query($query);
-				$this->obj->bind(":nama_shift",$nama_shift);
-				$this->obj->bind(":start_shift",$start_shift);
-				$this->obj->bind(":end_shift",$end_shift);
-				$this->obj->bind(":shift_id",$shift_id);
-				$this->obj->execute();
-			}
-			catch(PDOException $e)
-			{
-				echo $e->getMessage(); 
-			}
-		}
-		
-		public function hapus_shift($id_shift){
-			$query = "DELETE FROM shift WHERE id_karyawan=:id_shift";
-			$this->obj->query($query);
-			$this->obj->bind(":id_shift",$id_shift);
-			$this->obj->execute();
+	public function add_shift($nama_shift, $start_shift, $end_shift)
+	{
+		try
+		{
+			$query = "INSERT INTO absen(nama_shift, start_shift, end_shift) VALUES (:nama_shift, :start_shift, :end_shift)";
 			
-			header("Location: list_shift.php"); 
-		}
-		
-		public function tampil_shiftline($id_karyawan){
-			$query = "SELECT * FROM shift_line sl, shift s, karyawan k WHERE sl.id_shift = s.id_shift AND sl.id_karyawan = k.id_karyawan AND sl.id_karyawan=:id_karyawan
-			ORDER BY id_shiftline ASC";
 			$this->obj->query($query);
-			$this->obj->bind(':id_karyawan',$id_karyawan);
+			$this->obj->bind(':nama_shift',$nama_shift);
+			$this->obj->bind(':start_shift',$start_shift);
+			$this->obj->bind(':end_shift',$end_shift);
 			$this->obj->execute();
-			$data = $this->obj->resultset();
-			return $data;
 		}
-		
-		public function today_shiftline($karyawan_id, $tgl_skrg){
-			$query = "SELECT tanggal_shift, id_shift FROM shift_line WHERE id_karyawan=:karyawan_id AND tanggal_shift=:tgl_skrg ";
+		catch(PDOException $e)
+		{
+			echo $e->getMessage();
+		}    
+	}
+	
+	public function tampil_shift()
+	{
+		$query = "SELECT * FROM shift";
+		$this->obj->query($query);
+		$this->obj->execute();
+		$data = $this->obj->resultset();
+		return $data;
+	}
+	
+	function getID_shift($id_shift)
+	{
+		$query = "SELECT * FROM shift WHERE id_shift=:id_shift";
+		$this->obj->query($query);
+		$this->obj->bind(':id_shift',$id_shift);
+		$this->obj->execute();
+		$data = $this->obj->single();
+		return $data;
+	}
+
+	function edit_shift($shift_id, $nama_shift, $start_shift, $end_shift)
+	{
+		try
+		{
+			$query = "UPDATE shift SET nama_shift=:nama_shift, start_shift=:start_shift, end_shift=:end_shift WHERE id_shift=:shift_id";
 			$this->obj->query($query);
-			$this->obj->bind(':karyawan_id',$karyawan_id);
-			$this->obj->bind(':tgl_skrg',$tgl_skrg);
+			$this->obj->bind(":nama_shift",$nama_shift);
+			$this->obj->bind(":start_shift",$start_shift);
+			$this->obj->bind(":end_shift",$end_shift);
+			$this->obj->bind(":shift_id",$shift_id);
 			$this->obj->execute();
-			$data = $this->obj->single();
-			return $data;
 		}
-				
-		public function getAll_shiftline(){
-			$query = "SELECT * FROM shift_line";
-			$this->obj->query($query);
-			$this->obj->execute();
-			$data = $this->obj->resultset();
-			return $data;
+		catch(PDOException $e)
+		{
+			echo $e->getMessage(); 
 		}
 	}
+	
+	public function hapus_shift($id_shift)
+	{
+		$query = "DELETE FROM shift WHERE id_karyawan=:id_shift";
+		$this->obj->query($query);
+		$this->obj->bind(":id_shift",$id_shift);
+		$this->obj->execute();
+		
+		header("Location: list_shift.php"); 
+	}
+	
+	public function tampil_shiftline($id_karyawan)
+	{
+		$query = "SELECT * FROM shift_line sl, shift s, karyawan k WHERE sl.id_shift = s.id_shift AND sl.id_karyawan = k.id_karyawan AND sl.id_karyawan=:id_karyawan
+		ORDER BY id_shiftline ASC";
+		$this->obj->query($query);
+		$this->obj->bind(':id_karyawan',$id_karyawan);
+		$this->obj->execute();
+		$data = $this->obj->resultset();
+		return $data;
+	}
+	
+	public function today_shiftline($karyawan_id, $tgl_skrg)
+	{
+		$query = "SELECT tanggal_shift, id_shift FROM shift_line WHERE id_karyawan=:karyawan_id AND tanggal_shift=:tgl_skrg ";
+		$this->obj->query($query);
+		$this->obj->bind(':karyawan_id',$karyawan_id);
+		$this->obj->bind(':tgl_skrg',$tgl_skrg);
+		$this->obj->execute();
+		$data = $this->obj->single();
+		return $data;
+	}
+			
+	public function getAll_shiftline()
+	{
+		$query = "SELECT * FROM shift_line";
+		$this->obj->query($query);
+		$this->obj->execute();
+		$data = $this->obj->resultset();
+		return $data;
+	}
+}
 ?>
