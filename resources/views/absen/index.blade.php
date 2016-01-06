8fa62a1ed2fbe34bd634fc165d8348192a17dd1a
angga purnama <anggagewor@yahoo.com>
2016-01-06 11:41:05 +0700

    fix view absen

--- resources/views/absen/index.blade.php
+++ resources/views/absen/index.blade.php
@@ -36,13 +36,41 @@
 				<tbody>
 				@foreach ($absen as $row)
 				<?php
-					$tgl = explode(" ", $row->created_at);
+					$tgl 		= explode(" ", $row->created_at);
+					$timestamp 	= $row->created_at;
+					$tahun 		= date('Y',strtotime($timestamp));
+					$bulan 		= date('m',strtotime($timestamp));
+					$hari 		= date('d',strtotime($timestamp));
+					$jam 		= date('H:i:s',strtotime($timestamp));
+					$namaFoto	= $row->id_karyawan.'-'.$hari.'-'.$bulan.'-'.$tahun.'.jpeg';
+					$poto 		= 'uploads/'.$row->id_karyawan.'/'.$tahun.'/'.$bulan.'/'.$hari.'/'.$namaFoto;
 				?>
 					<tr>
 						<td>{{ laporanModel::getHari($tgl[0]) }}</td>
 						<td>{{ laporanModel::tgl_indo($tgl[0]) }}</td>
 						<td>{{ $tgl[1] }}</td>
-						<td>{{ $row->status }}</td>
+						<td>{{ $row->status }} 
+						@if($row->status == 'masuk')
+							<a href="#" data-toggle="modal" data-target="#data-{{ $row->id_absen }}"> [ Foto ] </a>
+							<!-- Modal -->
+							<div class="modal fade" id="data-{{ $row->id_absen }}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+							  <div class="modal-dialog" role="document">
+							    <div class="modal-content">
+							      <div class="modal-header">
+							        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+							        <h4 class="modal-title" id="myModalLabel">Foto {{ $row->created_at }}</h4>
+							      </div>
+							      <div class="modal-body">
+							        <img src="{{ url($poto) }}" class="text-center img-responsive img-thumbnail">
+							      </div>
+							      <div class="modal-footer">
+							        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+							      </div>
+							    </div>
+							  </div>
+							</div>
+						@endif
+						</td>
 					</tr>
 				@endforeach
 				</tbody>
